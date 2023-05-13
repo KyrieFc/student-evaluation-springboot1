64545c60eb175a2fc56a12abc885929cf63578a4
KyrieFc <frankchin99@sina.com>
2023-05-13 19:33:01 +0800

    StudentInfo interfaces and controller update

--- src/main/java/com/example/student_evaluation/controller/StudentInfoController.java
+++ src/main/java/com/example/student_evaluation/controller/StudentInfoController.java
@@ -4,15 +4,15 @@ import com.example.student_evaluation.common.CommonResult;
 import com.example.student_evaluation.entity.StudentInfo;
 import com.example.student_evaluation.entity.vo.StudentInfoQueryVO;
 import com.example.student_evaluation.entity.vo.StudentInfoVO;
-import com.example.student_evaluation.service.StudentInfoService;
+import com.example.student_evaluation.service.impl.StudentInfoServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 
@@ -21,7 +21,7 @@ import javax.validation.Valid;
 @RequestMapping("/api/info/student")
 public class StudentInfoController {
     @Autowired
-    private StudentInfoService studentInfoService;
+    private StudentInfoServiceImpl studentInfoService;
 
     @Operation(description = "查询学生信息")
     @GetMapping("/get")
